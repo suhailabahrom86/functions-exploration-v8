446110878f370ae46c93f4a829a1e7ebbca37d21
Suhaila Bahrom <157673640+suhailabahrom86@users.noreply.github.com>
2026-07-20 00:13:52 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCOR4aIqQQSM5hbJZfQlvVlScuDfMxDAkA",
+  authDomain: "functions-exploration-v8.firebaseapp.com",
+  projectId: "functions-exploration-v8",
+  storageBucket: "functions-exploration-v8.firebasestorage.app",
+  messagingSenderId: "849315176887",
+  appId: "1:849315176887:web:ed8771e617837215e44832"
+};
